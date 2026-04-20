f14c94a9cc1bf6d672e05f2361137356cd8b2b3f
Ye-Shaiyoe <akrommuhammadyusuf@gmail.com>
2026-04-20 15:55:55 +0700

    new layout and design upload area and revisi images,logo,etc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" href="{{ asset('images/BPSUML2.png') }}">
+        <link rel="icon" href="{{ asset('images/metrologi.png') }}">
 
-        <title>{{ config('app.name', 'BP SUML') }}</title>
+        <title>{{ $title ?? config('app.name', 'BP SUML') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
