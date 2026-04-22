85919125a11e578e34b3f0c75104d3f96a89f5a8
Ye-Shaiyoe <akrommuhammadyusuf@gmail.com>
2026-04-22 08:43:59 +0700

    Melakukan sedikit perubahan di 404 not found bisa beruopa file atau folder

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('errors.layout')
+
+@section('title', 'Halaman Tidak Ditemukan Saat Ini')
+@section('code', '404')
+@section('message', 'Halaman Tidak Ditemukan')
+@section('description', 'Maaf, kami tidak dapat menemukan halaman yang Anda cari. Mungkin halaman telah dipindahkan, dihapus, atau URL yang dimasukkan tidak tepat.')
