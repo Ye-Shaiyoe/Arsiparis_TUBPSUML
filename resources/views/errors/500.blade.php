96700a7660ea1dd26f5880651837b2c5407cc520
Ye-Shaiyoe <akrommuhammadyusuf@gmail.com>
2026-04-22 08:41:51 +0700

    Melakukan sedikit perubahan di 500 server error

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,6 @@
+@extends('errors.layout')
+
+@section('title', 'Kesalahan Server Internal ')
+@section('code', '500')
+@section('message', 'Kesalahan Server Internal')
+@section('description', 'Ups! Terjadi kesalahan di server kami. Kami sedang berusaha memperbaikinya secepat mungkin.')
