f4da3c6e561cc318022eeabe2bc89c8c67fe71d0
Ye-Shaiyoe <akrommuhammadyusuf@gmail.com>
2026-04-22 08:43:26 +0700

    Melakukan sedikit perubahan di 419 sesi anda terlah eraih dan harus refresf

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,6 @@
+@extends('errors.layout')
+
+@section('title', 'Sesi Berakhir Untuk Saat Ini')
+@section('code', '419')
+@section('message', 'Sesi Berakhir')
+@section('description', 'Sesi Anda telah berakhir karena tidak ada aktivitas dalam waktu lama. Silakan segarkan halaman dan coba lagi.')
